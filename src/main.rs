9672ea6f601b7eef4d0088af6d3287e270d97b0a
Flurin Feuerstein <ffeuerstein@gritec.ch>
2021-01-06 17:16:56 +0100

    added static file route for terminal

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+extern crate rocket_contrib;
 
-#[get("/")]
-fn index() -> String {
-    return "Hellooo";
-}
+use rocket_contrib::serve::StaticFiles;
 
-#[get("/screen/<port>/<speed>")]
-fn screen(port: String, speed: u16) -> String {
+#[get("/start/<port>/<speed>")]
+fn start(port: String, speed: u16) -> String {
     format!("Yaay, speed: {}, port: {}", speed, port)
 }
 
+#[get("/getOutput")]
+fn get_output() -> &'static str {
+    return "Here it is!!";
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, screen]).launch();
+    rocket::ignite()
+        .mount("/", StaticFiles::from("static") )
+        .mount("/api", routes![start, get_output])
+        .launch();
 }
